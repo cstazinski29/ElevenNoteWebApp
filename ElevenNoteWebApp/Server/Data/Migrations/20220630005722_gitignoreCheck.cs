a75644b2e06a52f2d417bcb2c16452e41044a7a9
cstazinski29 <cstazinski@gmail.com>
2022-06-29 20:58:16 -0400

    testing gitignore for migrations

---
+++ ElevenNoteWebApp/Server/Data/Migrations/20220630005722_gitignoreCheck.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElevenNoteWebApp.Server.Data.Migrations
+{
+    public partial class gitignoreCheck : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
